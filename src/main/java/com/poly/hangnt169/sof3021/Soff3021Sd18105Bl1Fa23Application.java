95cbda313ddc3c7ca5b02919ffe6bae558182cb0
thuyduong203 <nguyenthithuyduong948@gmail.com>
2023-09-10 03:13:05 +0700

    SOFF3021_SD18105_BL1_FA23

---
+++ src/main/java/com/poly/hangnt169/sof3021/Soff3021Sd18105Bl1Fa23Application.java
@@ -0,0 +1,13 @@
+package com.poly.hangnt169.sof3021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soff3021Sd18105Bl1Fa23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soff3021Sd18105Bl1Fa23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/hangnt169/sof3021/Soff3021Sd18105Bl1Fa23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.hangnt169.sof3021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soff3021Sd18105Bl1Fa23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
